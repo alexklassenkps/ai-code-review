45b39fa849d25c6aa21098e498855690a2588f30
Alexander Klassen <alexander.klassen@kps.com>
2026-02-14 23:50:38 +0100

    context files

--- dist/index.js
+++ dist/index.js
@@ -25848,13 +25848,13 @@ function loadContextFilesWithStatus(filePaths, workDir) {
     for (const filePath of filePaths) {
         const fullPath = path.resolve(workDir, filePath);
         try {
-            console.info(`trying to load file at "${filePath}"`)
+            console.info(`trying to load file at "${fullPath}"`)
             const content = fs.readFileSync(fullPath, 'utf8');
             sections.push(`## ${filePath}\n\n${content}`);
             includedFiles.push(filePath);
         } catch (e) {
             missingFiles.push(filePath);
-            console.info(`could not load "${filePath}"`)
+            console.info(`could not load "${fullPath}"`)
         }
     }
 
